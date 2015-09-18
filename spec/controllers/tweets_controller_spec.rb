c1020cfd401b48c768b82aa54ede9ab70affa518
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2015-09-18 18:48:26 +0900

    Create controller_spec template

---
+++ spec/controllers/tweets_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe TweetsController, type: :controller do
+  describe 'GET #index' do
+  end
+
+  describe 'GET #new' do
+  end
+
+  describe 'POST #create' do
+  end
+
+  describe 'DELETE #destory' do
+  end
+
+  describe 'GET #edit' do
+  end
+
+  describe 'PUT #update' do
+  end
+end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe UsersController, type: :controller do
+  describe 'GET #show' do
+  end
+end

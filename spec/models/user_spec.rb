c976f838d02c05dc8579b2f3b5fa4d6da2f90536
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2015-09-18 18:25:58 +0900

    Using FactoryGirl for test user instance

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,48 +2,36 @@ require 'rails_helper'
 
 describe User, type: :model do
   it 'is invalid without a nickname' do
-    user = User.new()
+    user = build(:user, nickname: nil)
     user.valid?
     expect(user.errors[:nickname]).to include("can't be blank")
   end
 
   it 'is invalid without a email' do
-    user = User.new()
+    user = build(:user, email: nil)
     user.valid?
     expect(user.errors[:email]).to include("can't be blank")
   end
 
   it 'is invalid without a password' do
-    user = User.new()
+    user = build(:user, password: nil)
     user.valid?
     expect(user.errors[:password]).to include("can't be blank")
   end
 
   it 'is invalid when nickname length more than 6' do
-    user1 = User.new(
-      nickname: '123456',
-      email: 'test@we-b.co.jp',
-      password: 'password'
-    )
+    user1 = build(:user, nickname: Faker::Internet.user_name(6..6))
     expect(user1).to be_valid
 
-    user2 = User.new(
-      nickname: '1234567'
-    )
+    user2 = build(:user, nickname: Faker::Internet.user_name(7..7))
     user2.valid?
     expect(user2.errors[:nickname]).to include('is too long (maximum is 6 characters)')
   end
 
   it 'is invalid with a duplicate email address' do
-    User.create(
-      nickname: 'Shinbo',
-      email: 'test@we-b.co.jp',
-      password: 'password'
-    )
-    user = User.new(
-      email: 'test@we-b.co.jp'
-    )
-    user.valid?
-    expect(user.errors[:email]).to include('has already been taken')
+    user1 = create(:user)
+    user2 = build(:user, email: user1.email)
+    user2.valid?
+    expect(user2.errors[:email]).to include('has already been taken')
   end
 end
